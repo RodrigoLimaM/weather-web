ab04b1999ead1c7970a89ec577c38812fb4a5915
RodrigoLimaM <digo100200@gmail.com>
2020-08-17 22:17:24 -0300

    init

---
+++ src/main/java/com/weather/WeatherWebApplication.java
@@ -0,0 +1,13 @@
+package com.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWebApplication.class, args);
+	}
+
+}
